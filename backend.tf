32ce978c9550d5257cc763dad41a57a865be925c
shajahan-sayed <shajahansayed111@gmail.com>
2025-11-27 20:15:27 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,10 +1,10 @@
 terraform {
    backend "s3" {
-      bucket_name    = "backend10775"
+      bucket         = "backend10775"
       key            = "ec2/terraform.tfstate"
       dynamodb_table = "backend2"
       encrypt        = true
-      region         = var.aws_region
+      region         = "ap-southeast-2"
   }
 }
       
